9d5810fbbf55ad0fe4dff653618ed0f5e399e6c5
jlinares1 <jlinares1@inscamidemar.cat>
2025-02-11 16:48:16 +0100

    git pull serverws

---
+++ Server-ws/server/index.js
@@ -0,0 +1,45 @@
+import express from 'express'
+import logger from 'morgan'
+
+import { Server } from 'socket.io';
+import {createServer} from 'node:http';
+
+
+const port = process.env.PORT ?? 8100;
+const app = express();
+const server = createServer(app)
+const io = new Server(server,{
+    connectionStateRecovery:{}
+});
+
+
+io.on('connection',(socket )=>{
+    console.log("user connected")
+    socket.on('disconnect',()=>{
+        console.log("user disconected")
+    })
+    socket.on('message',(mensage)=>{
+        console.log("mensaje "+mensage)
+        io.emit('message',mensage)
+        
+    })
+    socket.on('error',(error)=>{
+        console.log("error "+error)
+    })
+})
+
+
+//logger
+app.use(logger('dev'))
+
+//route
+app.get('/',(req,res) =>{
+    res.sendFile(process.cwd()+'/client/index.html');
+
+})
+
+
+//listen
+server.listen(port,()=>{
+    console.log(`server running on port ${port}`)
+})
