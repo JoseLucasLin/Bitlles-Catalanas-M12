d10e8a1cc04bd1903cda7e9f24f2f6041d1d8dea
Areikusu <aleix18122005@gmail.com>
2025-02-20 16:12:47 +0100

    .

--- database/migrations/2025_02_18_154048_role.php
+++ database/migrations/2025_02_18_154048_role.php
@@ -3,19 +3,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRolesTable extends Migration
+return new class extends Migration
 {
-    public function up()
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
     {
-        Schema::dropIfExists('Roles');
-        Schema::create('Roles', function (Blueprint $table) {
-            $table->id()-> primary() ->autoIncrement();
-            $table->string('role_name', 40);
+        Schema::create('role', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
         });
     }
 
-    public function down()
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
     {
-        Schema::dropIfExists('Roles');
+        Schema::dropIfExists('role');
     }
-}
+};
