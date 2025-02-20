a6403793914b30cb0ca8b1d1a11448d8334b4f5c
Areikusu <aleix18122005@gmail.com>
2025-02-20 17:53:50 +0100

    migrations_upadate

--- database/seeders/RoleSeed.php
+++ database/seeders/RoleSeed.php
@@ -5,16 +5,16 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class RolesSeed extends Seeder
+class RoleSeed extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        DB::table('roles')->insert([
-            ['role_name' => 'arbitro'],
-            ['role_name' => 'admin'],
+        DB::table('Roles')->insert([
+            ['name' => 'arbitro'],
+            ['name' => 'admin'],
         ]);
     }
 }
