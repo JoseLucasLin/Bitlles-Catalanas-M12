7271b46d798c14a64b4bbd696d0382e881fd42ba
jlinares1 <jlinares1@inscamidemar.cat>
2025-02-20 17:32:47 +0100

    Tornament with out seeder

---
+++ database/migrations/2025_02_20_162504_tournament.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        /*
+            id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    type INTEGER NOT NULL,
+    normal_price FLOAT NOT NULL,
+    partner_price FLOAT NOT NULL,
+    image VARCHAR(100) DEFAULT 'default_image.png',
+    expected_date VARCHAR(50),
+    start_date TIMESTAMP,
+    end_date TIMESTAMP */
+        Schema::dropIfExists('Tournament');
+        Schema::create('Tournament', function (Blueprint $table) {
+            $table->id()->primary();
+            $table->string('name',255)->nullable(false);
+            $table->integer('type')->nullable(false);
+            $table->float('normal_price')->nullable(true);
+            $table->float('partner_price')->nullable(true);
+            $table->string('image',250)->default('image.png');
+            $table->timestamp('expected_date');
+            $table->timestamp('start_date');
+            $table->timestamp('end_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+        Schema::dropIfExists('Tournament');
+    }
+};
---
+++ database/seeders/TournamentSeed.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TournamentSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        /*
+            id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    type INTEGER NOT NULL,
+    normal_price FLOAT NOT NULL,
+    partner_price FLOAT NOT NULL,
+    image VARCHAR(100) DEFAULT 'default_image.png',
+    expected_date VARCHAR(50),
+    start_date TIMESTAMP,
+    end_date TIMESTAMP
+     */
+    }
+}
