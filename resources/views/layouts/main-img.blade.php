3c69b7e3c90f58babc83022b7b3aa7abd05f3ef3
Yuriy <yurapro1129@gmail.com>
2025-02-24 13:45:20 +0100

    fix archives

---
+++ resources/views/layouts/main-img.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <img src="{{ asset('main-img/bitlles.jpg') }}" alt="">
+</div>
--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -4,12 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Bitlles Catalanes</title>
     @vite(['resources/css/app.css'])
 </head>
 <body>
+
+    @include('layouts.nav')
+
+    @include('layouts.main-img')
+
     <h1 class="text-3xl font-bold underline">
         ¡Bienvenido a mi proyecto Laravel!
     </h1>
+
+    @include('layouts.footer')
+
 </body>
+
+<script>
+    const mobileMenuButton = document.getElementById('mobile-menu-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    mobileMenuButton.addEventListener('click', () => {
+      mobileMenu.classList.toggle('hidden');
+    });
+</script>
+
 </html>
