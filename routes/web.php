7242659f9bd75feca25ed7361f9c4f9adf2d59d7
jlinares1 <jlinares1@inscamidemar.cat>
2025-03-10 16:30:29 +0100

    auth commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 use App\Http\Controllers\ServerController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -26,6 +27,8 @@ Route::get('/admin/add-players', function () {
 
 Route::get('/p', [ServerController::class, 'index']);
 
+
+
 //ruta cambio idioma
 Route::get('locale/{locale}', function ($locale) {
     if (in_array($locale, config('app.available_locales'))) {
@@ -34,3 +37,19 @@ Route::get('locale/{locale}', function ($locale) {
     }
     return redirect()->back();
 })->name('locale.change');
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
